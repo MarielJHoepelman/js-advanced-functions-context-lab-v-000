acad7d4bdf76b7d7332677380daa83748f100beb
Mariel Hoepelman <marielhoepelman@gmail.com>
2020-10-15 19:25:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,6 +18,8 @@ const createEmployeeRecord = (employeeData) => {
     timeOutEvents
   };
 }
+
+
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
