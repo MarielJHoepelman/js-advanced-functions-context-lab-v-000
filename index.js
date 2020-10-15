508f902bedbb475c4e57841e5998047059a73336
Mariel Hoepelman <marielhoepelman@gmail.com>
2020-10-15 19:34:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const createEmployeeRecord = employeeData => {
 const createEmployeeRecords = records => {
   let arrarOfObjects = [];
   for (const record of records) {
-    createEmployeeRecords(record)
+    createEmployeeRecords(record);
   }
   return arrarOfObjects;
 }
